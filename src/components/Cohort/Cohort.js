13040f463b6db574a5bf9f6cf837a67fc412bee1
srslie <aliceruppert@gmail.com>
2021-02-02 01:24:30 -0700

    Create basic member info card

--- src/components/Cohort/Cohort.js
+++ src/components/Cohort/Cohort.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Person from '../Person/Person';
 import './Cohort.css';
 
-// COHORT COMPONENT CODE GOES HERE
+const Cohort = (props) => {
+  return props.staff.map(person => {
+    console.log(person.name)
+    return (
+      <div>
+        <img src={person.photo}/>
+        <h1 className="name">{person.name}</h1>
+        <h2>"{person.quote}"</h2>
+        <h3>{person.superlative}</h3>
+      </div>
+    )
+  })
+}
 
 export default Cohort;
